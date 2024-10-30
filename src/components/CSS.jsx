5d3c36fb95bcb1715a0914cc4385c155fe4a8b3f
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2024-10-30 17:36:42 +0530

    update

--- src/components/CSS.jsx
+++ src/components/CSS.jsx
@@ -10,16 +10,16 @@ function CSS() {
     <div className="flex flex-col md:flex-row">
       {/* Sidebar */}
       <div className="w-full md:w-1/4 bg-gray-100 p-4 md:fixed md:h-screen md:overflow-y-auto">
-        <h2 className="text-xl font-bold mb-4">Git Topics</h2>
+        <h2 className="text-xl font-bold mb-4">CSS Topics</h2>
         <ul className="space-y-2">
-          <Topics name="Git FAQs"/>
+          <Topics name="CSS FAQs"/>
         </ul>
       </div>
       <Support/>
 
       {/* Main content */}
       <div className="w-full md:w-3/4 p-4 md:ml-[25%] overflow-y-auto">
-        <h1 className="text-3xl font-bold mb-6">Learn Git</h1>
+        <h1 className="text-3xl font-bold mb-6">Learn CSS</h1>
         {CssData.map((section)=>(
           <Section
             id={section.id}
