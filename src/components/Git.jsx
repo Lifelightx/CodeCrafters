636fa2b5eaf878a09198e705d8016b8217fcb5e2
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2024-10-30 11:42:34 +0530

    update

--- src/components/Git.jsx
+++ src/components/Git.jsx
@@ -9,30 +9,30 @@ function Git() {
     const sectionData = GitData;
   return (
     <div className="flex flex-col md:flex-row">
-    {/* Sidebar */}
-    <div className="w-1/4 bg-gray-100 p-4 fixed h-screen overflow-y-auto">
-      <h2 className="text-xl font-bold mb-4">Git Topics</h2>
-      <ul className="space-y-2">
-        <Topics name="Git FAQs"/>
-      </ul>
-    </div>
-    <Support/>
+      {/* Sidebar */}
+      <div className="w-full md:w-1/4 bg-gray-100 p-4 md:fixed md:h-screen md:overflow-y-auto">
+        <h2 className="text-xl font-bold mb-4">Git Topics</h2>
+        <ul className="space-y-2">
+          <Topics name="Git FAQs"/>
+        </ul>
+      </div>
+      <Support/>
 
-    {/* Main content */}
-    <div className="w-3/4 p-4 ml-[25%] overflow-y-auto">
-    <h1 className="text-3xl font-bold mb-6">Learn Git</h1>
-    {sectionData.map((section)=>(
-        <Section
-        id={section.id}
-        topic={section.topic}
-        content={section.content}
-        code={section.code}
-        language={section.language}
-        />
-    ))}
-    <Footer/>
+      {/* Main content */}
+      <div className="w-full md:w-3/4 p-4 md:ml-[25%] overflow-y-auto">
+        <h1 className="text-3xl font-bold mb-6">Learn Git</h1>
+        {sectionData.map((section)=>(
+          <Section
+            id={section.id}
+            topic={section.topic}
+            content={section.content}
+            code={section.code}
+            language={section.language}
+          />
+        ))}
+        <Footer/>
+      </div>
     </div>
-  </div>
   )
 }
 
--- src/components/JavaScript.jsx
+++ src/components/JavaScript.jsx
@@ -9,9 +9,9 @@ const sectionsData = JavaScriptData;
 
 function JavaScript() {
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row">
       {/* Sidebar */}
-      <div className="w-1/4 bg-gray-100 p-4 fixed h-screen overflow-y-auto">
+      <div className="w-full md:w-1/4 bg-gray-100 p-4 md:fixed md:h-screen md:overflow-y-auto">
         <h2 className="text-xl font-bold mb-4">JavaScript Topics</h2>
         <ul className="space-y-2">
           <Topics name='Js Questions '/>
@@ -20,7 +20,7 @@ function JavaScript() {
       </div>
       <Support/>
       {/* Main content */}
-      <div className="w-3/4 p-4 ml-[25%] overflow-y-auto">
+      <div className="w-full md:w-3/4 p-4 md:ml-[25%] overflow-y-auto">
         <h1 className="text-3xl font-bold mb-6">Learn JavaScript</h1>
         {sectionsData.map((section, index) => (
           <Section
